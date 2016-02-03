4e32536c7031a29ddb88a201c32d100d3c0b48f1
Romuald Le Brun <Romuald Le Brun>
2016-02-03 16:52:06 +0100

    v0.5

--- src/lobby/CmdManager.java
+++ src/lobby/CmdManager.java
@@ -6,23 +6,34 @@
 package lobby;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 /**
  *
  * @author Romuald
  */
-public class CmdManager {
+public class CmdManager implements CommandExecutor {
     
-    public void InstanceSkyWars(Player p) {
-        //Plugin test = Bukkit.getPluginManager().getPlugin("SkyWars");
-        //test.onEnable();
-        Bukkit.dispatchCommand(p, "skywars");
-    }
+    private Location spawn_start = new Location(Bukkit.getWorld("World"), 0.5, 101, 0.5);
+    private Location choice_class = new Location(Bukkit.getWorld("World"), 500.5, 101, 500.5);
     
-    public void addSkyWars(Player p) {
-        //Plugin test = Bukkit.getPluginManager().getPlugin("SkyWars");
-        p.sendMessage("Ok pour la commands");
-        Bukkit.dispatchCommand(p, "skywars enter");
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player p = (Player) sender;
+        
+        if(cmd.getName().equalsIgnoreCase("hub") && sender instanceof Player) {
+            p.teleport(spawn_start);
+            p.sendMessage(ChatColor.RED+p.getName()+ChatColor.RESET+" Téléporter au spawn");
+        }
+        else if(cmd.getName().equalsIgnoreCase("pvp") && sender instanceof Player) {
+            p.teleport(choice_class);
+            p.sendMessage(ChatColor.RED+p.getName()+ChatColor.RESET+" Téléporter au lobby PVP");
+        }
+        return false;
     }
 }
